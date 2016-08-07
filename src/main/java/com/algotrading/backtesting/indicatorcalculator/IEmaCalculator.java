a00ff6182e241e9164b6c54ad71cc43f5864f2c8
kw.ho <hokawing1831@gmail.com>
2016-08-07 15:09:55 +0800

    [IMP] Clean Up IStock Related stuff coz use Stock later
    Issue: 5

--- src/main/java/com/algotrading/backtesting/indicatorcalculator/IEmaCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.algotrading.backtesting.indicatorcalculator;
-
-import java.math.BigDecimal;
-import java.util.Date;
-
-public interface IEmaCalculator {
-	public BigDecimal calculate(Date date, int scale);
-}
