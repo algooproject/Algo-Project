1e46fe6ddf6dd9271ff8fcc32cd6a5d12e657ab7
kw.ho <hokawing1831@gmail.com>
2016-08-07 15:46:57 +0800

    [IMP] Implement StockHistory class
    Issue: 7

---
+++ src/main/java/com/algotrading/backtesting/stock/StockHistory.java
@@ -0,0 +1,43 @@
+package com.algotrading.backtesting.stock;
+
+import java.util.Date;
+
+public class StockHistory {
+
+	private Date date;
+	private double high;
+	private double low;
+	private double open;
+	private double close;
+	private double adjClose;
+	private double volume;
+
+	public Date getDate() {
+		return date;
+	}
+
+	public double getHigh() {
+		return high;
+	}
+
+	public double getLow() {
+		return low;
+	}
+
+	public double getOpen() {
+		return open;
+	}
+
+	public double getClose() {
+		return close;
+	}
+
+	public double getAdjClose() {
+		return adjClose;
+	}
+
+	public double getVolume() {
+		return volume;
+	}
+
+}
