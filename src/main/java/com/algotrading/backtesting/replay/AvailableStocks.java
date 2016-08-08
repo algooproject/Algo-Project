8d5db151b20f83cd9ca440d11b4f83cfc93daa8c
kw.ho <hokawing1831@gmail.com>
2016-08-08 22:11:44 +0800

    [IMP] Implement AvailableStocks Class
    Issue: 32

---
+++ src/main/java/com/algotrading/backtesting/replay/AvailableStocks.java
@@ -0,0 +1,38 @@
+package com.algotrading.backtesting.replay;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.algotrading.backtesting.stock.Stock;
+
+public class AvailableStocks {
+
+	private List<Stock> stocks;
+
+	public AvailableStocks(String fileList) throws IOException, ParseException {
+		read(fileList);
+	}
+
+	public void read(String filePath) throws IOException, ParseException {
+		Path file = new File(filePath).toPath();
+		Charset charset = Charset.defaultCharset();
+		List<String> stringList = Files.readAllLines(file, charset);
+		stocks = new ArrayList<>();
+		for (String line : stringList) {
+			Stock stock = new Stock(line);
+			stock.read();
+			stocks.add(stock);
+		}
+	}
+
+	public List<Stock> get() {
+		return stocks;
+	}
+
+}
