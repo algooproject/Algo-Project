45c2b978b1192cdcf7d2655e3eeb314db7301eb0
kw.ho <hokawing1831@gmail.com>
2016-08-08 22:23:37 +0800

    [IMP] Implement replay class
    Issue: 15

---
+++ src/main/java/com/algotrading/backtesting/replay/Replay.java
@@ -0,0 +1,48 @@
+package com.algotrading.backtesting.replay;
+
+import java.util.Date;
+
+import com.algotrading.backtesting.portfolio.Portfolio;
+import com.algotrading.backtesting.portfolio.PortfolioComponent;
+import com.algotrading.backtesting.stock.PortfolioHistory;
+import com.algotrading.backtesting.stock.Stock;
+import com.algotrading.backtesting.strategy.Strategies;
+
+public class Replay {
+	private Date startDate;
+	private Date endDate;
+	private PortfolioHistory portfolioHistory;
+	private Strategies strategies;
+	private AvailableStocks availableStocks;
+	private TradingDate tradingDate;
+
+	public Replay(Date startDate, Date endDate, PortfolioHistory portfolioHistory, Strategies strategies,
+			AvailableStocks availableStocks, TradingDate tradingDate) {
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.portfolioHistory = portfolioHistory;
+		this.strategies = strategies;
+		this.availableStocks = availableStocks;
+		this.tradingDate = tradingDate;
+	}
+
+	public void simulate() {
+		Date currentDate = startDate;
+		while (!tradingDate.isLastDate() && tradingDate.currentDate().compareTo(endDate) < 0) {
+			currentDate = tradingDate.currentDate();
+			Portfolio portfolio = new Portfolio(currentDate);
+			for (Stock stock : availableStocks.get()) {
+				PortfolioComponent component = strategies.buySellAmount(stock, currentDate);
+				if (component.getQuantity() != 0) {
+					portfolio.add(component);
+				}
+			}
+			portfolioHistory.put(currentDate, portfolio);
+			tradingDate.rollDay();
+		}
+	}
+
+	public PortfolioHistory getPortfolioHistory() {
+		return portfolioHistory;
+	}
+}
