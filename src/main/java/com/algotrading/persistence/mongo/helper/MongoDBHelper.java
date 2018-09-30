118a37381eaa38e2465f1ec1ed29b4c076ccdbf7
kw.ho <hokawing1831@gmail.com>
2018-10-01 00:38:27 +0800

    [IMP] MongoDBHelper

--- src/main/java/com/algotrading/persistence/mongo/helper/MongoDBHelper.java
+++ src/main/java/com/algotrading/persistence/mongo/helper/MongoDBHelper.java
@@ -13,6 +13,8 @@ public class MongoDBHelper {
 
 	public final DummyTickPriceDBHelper dummyTickPrice;
 	public final DailyPriceHelper dailyPrice;
+	public final StockInfoHelper stockInfo;
+	public final TickPriceHelper tickPrice;
 
 	public MongoDBHelper(Logger logger, String databaseName) {
 		this(logger, "localhost", 27017, databaseName);
@@ -24,6 +26,9 @@ public class MongoDBHelper {
 		this.mongoDbLogger = new MongoDBLogger(logger);
 		this.dummyTickPrice = new DummyTickPriceDBHelper(database, mongoDbLogger);
 		this.dailyPrice = new DailyPriceHelper(database, mongoDbLogger);
+		this.stockInfo = new StockInfoHelper(database, mongoDbLogger);
+		this.tickPrice = new TickPriceHelper(database, mongoDbLogger);
+
 	}
 
 	public void dropDatabase() {
